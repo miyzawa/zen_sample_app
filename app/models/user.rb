241af7702ff743d72ec0de8572910330241a883f
miyzawa <b1504527@planet.kanazawa-it.ac.jp>
2017-05-05 06:45:50 +0200

    email属性に制約

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
+  validates :email, presence: true, length: [ maximum: 50 ], format: [ with: VALID_EMAIL_REGEX ], uniqueness: { case_sensitive: false }
 end
